232650ea30180bd59621b4fc78b9f3e3bfd33351
zhongq111 <2269831144@qq.com>
2025-02-21 15:07:19 +0800

    Add files via upload

---
+++ BiYeSheJi.API/BiYeSheJi.Entity/StuEntity/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiYeSheJi.Entity.StuEntity
+{
+    //帖子类
+    public class Post
+    {
+        public string Title { get; set; } //标题
+        public string Content { get; set; } //内容
+        public string Author { get; set; } //作者
+
+    }
+}
